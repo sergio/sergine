c9981c1d75c6c7fd8aa7812847f6f57df65c2153
Sergio Acosta <sergio.acosta@gmail.com>
2016-06-29 23:22:24 -0500

    Refactor parser implementation to use FParsec

--- source/Sergine/Parser.fs
+++ source/Sergine/Parser.fs
@@ -1,12 +1,16 @@
 module Parser
 
 open CommonTypes
+open FParsec.CharParsers
+open FParsec.Primitives
 
 let parseFen (fen : string) : Result<Position, string> =
-    if(fen.Contains(" w ")) then
-        Success { PlayerInTurn = White }
-    else if(fen.Contains(" b ")) then
-        Success { PlayerInTurn = Black }
-    else
-        Failure "Player in turn is neither 'w' nor 'b'."
-
+    let playerBetweenSpaces = spaces1 >>. (pchar 'w' <|> pchar 'b') .>> spaces1
+    let pposition = many (skipAnyOf "pnbrqkPNBRQK12345678/") >>. playerBetweenSpaces
+    let result = run pposition fen
+    match result with
+    | Success (result, _, _) -> 
+        match result with
+        | 'w' -> Result.Success { PlayerInTurn = White }
+        | _ -> Result.Success { PlayerInTurn = Black }
+    | Failure (errorMessage, _, _) -> Result.Failure errorMessage 
