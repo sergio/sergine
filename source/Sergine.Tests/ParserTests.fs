e5f31e5729a1349105d7413db9e7fc707af9730c
Sergio Acosta <sergio.acosta@gmail.com>
2016-07-04 15:22:51 -0500

    Castling options subparser

--- source/Sergine.Tests/ParserTests.fs
+++ source/Sergine.Tests/ParserTests.fs
@@ -5,7 +5,13 @@ open Swensen.Unquote
 open CommonTypes
 open Parser
 
+let isParsingFailure(result) = 
+    match result with
+    | Failure s -> true
+    | _ -> false
+
 module ``When parsing the player in turn`` =
+
     [<Test>]
     let ``returns White when 'w'`` () =
         let playerInTurn = parseTurn "w"
@@ -19,8 +25,34 @@ module ``When parsing the player in turn`` =
     [<Test>]
     let ``returns failure when not 'w' nor 'b'`` () =
         let result = parseTurn "x"
-        test <@ match result with
-                | Failure s -> true
-                | _ -> false
-            @>
+        test <@ isParsingFailure result @>
 
+module ``When parsing castling options`` =
+
+    [<Test>]
+    let ``returns empty list for '-'`` () =
+        let result = parseCastlings "-"
+        test <@ result = Success [] @>
+
+    [<Test>]
+    let ``returns queenside white for 'Q'`` () =
+        let result = parseCastlings "Q"
+        test <@ result = Success [{Side = Queenside; Player = White }] @>
+
+    [<Test>]
+    let ``returns all options for 'QKqk'`` () =
+        let result = parseCastlings "QKqk"
+        let expectedCastlings = Set.ofList [
+                                            {Player = White; Side = Queenside};
+                                            {Player = Black; Side = Queenside};
+                                            {Player = White; Side = Kingside};
+                                            {Player = Black; Side = Kingside}
+                                           ]
+        test <@ match result with
+                | Success castlings -> castlings |> Set.ofList = expectedCastlings
+                | Failure _ -> false @>
+
+    [<Test>]
+    let ``returns error if unknown input`` () =
+        let result = parseCastlings "QkA"
+        test <@ isParsingFailure result @>
--- source/Sergine/Parser.fs
+++ source/Sergine/Parser.fs
@@ -13,14 +13,33 @@ open FParsec.Primitives
 //     | Success (result, _, _) -> Result.Success { Turn = result } 
 //     | Failure (errorMessage, _, _) -> Result.Failure errorMessage 
 
+// Sub-parsers
+
 let whitePlayer = charReturn 'w' White
 let blackPlayer = charReturn 'b' Black
 
 let pturn : Parser<Player, unit> = whitePlayer <|> blackPlayer  
 
+let pCastlings : Parser<AvailableCastling list, unit> =
+    (charReturn '-' [] 
+    <|> many (
+            charReturn 'K' { Player = White; Side = Kingside }
+        <|> charReturn 'Q' { Player = White; Side = Queenside }
+        <|> charReturn 'k' { Player = Black; Side = Kingside }
+        <|> charReturn 'q' { Player = Black; Side = Queenside }
+        )
+    )
+
+// Sub-parser testing functions
+
 let parseTurn (s : string) : Result<Player, string> = 
-    let r = run pturn s
+    let r = run (pturn .>> eof) s
     match r with
     | Success (player, _, _) -> Result.Success player
     | Failure (msg, _, _) -> Result.Failure msg
 
+let parseCastlings (s: string) : Result<AvailableCastling list, string> =
+    let r = run (pCastlings .>> eof) s
+    match r with
+    | Success (castlings, _, _) -> Result.Success castlings
+    | Failure (msg, _, _) -> Result.Failure msg
