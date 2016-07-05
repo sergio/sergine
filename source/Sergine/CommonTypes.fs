4c113256ff41b7bb8bd98cab5b2198e779b671c4
Sergio Acosta <sergio.acosta@gmail.com>
2016-07-05 15:40:12 -0500

    Removed unused type declaration.

--- source/Sergine/CommonTypes.fs
+++ source/Sergine/CommonTypes.fs
@@ -3,12 +3,17 @@ module CommonTypes
 type Result<'TSuccess,'TFailure> = | Success of 'TSuccess | Failure of 'TFailure
 
 type Player = | Black | White
+
 type PieceKind = | Pawn | Knight | Bishop | Rook | Queen | King
+
 type Piece = { Player : Player; Kind: PieceKind }
-type Square = Piece option
+
 type CastlingSide = | Queenside | Kingside 
+
 type AvailableCastling = { Player: Player; Side: CastlingSide }
+
 type Coordinate = int * int
+
 type Position = {
     Board: Piece option list list;
     Turn: Player;
