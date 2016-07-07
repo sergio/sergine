90a14995b1a0c34d244523d04c744a7992246069
Sergio Acosta <sergio.acosta@gmail.com>
2016-07-07 10:07:42 -0500

    Functions to translate between coordinates and algebraic notation

--- source/Sergine/CommonTypes.fs
+++ source/Sergine/CommonTypes.fs
@@ -22,3 +22,16 @@ type Position = {
     HalfmoveCounter: int;
     FullmoveCounter: int
 }
+
+module Algebraic =
+
+    let ofCoordinate ((f, r) : Coordinate) =
+        let fileName = char(int('a') + f)
+        let rankName = char(int('1') + r)
+        sprintf "%c%c" fileName rankName
+
+    let toCoordinate (a : string) : Coordinate =
+        let fileIndex = int(a.[0]) - int('a')
+        let rankIndex = int(a.[1]) - int('1')
+        (fileIndex, rankIndex)
+
