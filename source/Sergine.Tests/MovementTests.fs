82a317aa35852c13ab4fa2f6d59d11ea52f1c3b3
Sergio Acosta <sergio.acosta@gmail.com>
2016-07-12 11:39:25 -0500

    Generate moves for Queen on empty board

--- source/Sergine.Tests/MovementTests.fs
+++ source/Sergine.Tests/MovementTests.fs
@@ -62,3 +62,22 @@ module ``Bishop movement`` =
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["a1";"b2";"c3";"d4";"e5";"f6";"g7"]
             test <@ actualMoves = expectedMoves @>
+
+module ``Queen movement`` =
+
+    module ``When board is empty`` =
+        [<Test>]
+        let ``Returns correct moves from e3`` () =
+
+            let source, piece = "e3", { Player = White; Kind = Queen }
+            let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
+            let expectedMoves = movesToTargetSquares source piece (["c1";"d2";"f4";"g5";"h6";"a7";"b6";"c5";"d4";"f2";"g1"] @ ["e1"; "e2"; "e4"; "e5"; "e6"; "e7"; "e8"; "a3"; "b3"; "c3"; "d3"; "f3"; "g3"; "h3"])
+            test <@ actualMoves = expectedMoves @>
+
+        [<Test>]
+        let ``Returns correct moves from h8`` () =
+
+            let source, piece = "h8", { Player = White; Kind = Queen }
+            let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
+            let expectedMoves = movesToTargetSquares source piece (["a1";"b2";"c3";"d4";"e5";"f6";"g7"] @ ["h7";"h6";"h5";"h4";"h3";"h2";"h1";"g8";"f8";"e8";"d8";"c8";"b8";"a8"])
+            test <@ actualMoves = expectedMoves @>
