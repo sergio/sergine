5dc75a519e636d31f7c03e09835ceef91a892441
Sergio Acosta <sergio.acosta@gmail.com>
2016-07-12 12:33:42 -0500

    Helper test method to improve readability

--- source/Sergine.Tests/MovementTests.fs
+++ source/Sergine.Tests/MovementTests.fs
@@ -5,9 +5,10 @@ open Movement
 open NUnit.Framework
 open Swensen.Unquote
 
-let a p k = Some { Player = p; Kind = k }
 let coord = Algebraic.toCoordinate
 let alg = Algebraic.ofCoordinate
+let at = ()
+let a player kind at square = (square, { Player = player; Kind = kind})
 
 let movesToTargetSquares source piece targetSquares =
     targetSquares
@@ -30,14 +31,14 @@ module ``Rook movement`` =
     module ``When board is empty`` =
         [<Test>]
         let ``Returns correct moves from e3`` () =
-            let source, piece = "e3", { Player = White; Kind = Rook }
+            let source, piece = a White Rook at "e3"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["e1"; "e2"; "e4"; "e5"; "e6"; "e7"; "e8"; "a3"; "b3"; "c3"; "d3"; "f3"; "g3"; "h3"] 
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Returns correct moves from h8`` () =
-            let source, piece = "h8", { Player = White; Kind = Rook }
+            let source, piece = a White Rook at "h8"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["h7";"h6";"h5";"h4";"h3";"h2";"h1";"g8";"f8";"e8";"d8";"c8";"b8";"a8"]
             test <@ actualMoves = expectedMoves @>
@@ -47,14 +48,14 @@ module ``Bishop movement`` =
     module ``When board is empty`` =
         [<Test>]
         let ``Returns correct moves from e3`` () =
-            let source, piece = "e3", { Player = White; Kind = Bishop }
+            let source, piece = a White Bishop at "e3"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["c1";"d2";"f4";"g5";"h6";"a7";"b6";"c5";"d4";"f2";"g1"]
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Returns correct moves from h8`` () =
-            let source, piece = "h8", { Player = White; Kind = Bishop }
+            let source, piece = a White Bishop at "h8"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["a1";"b2";"c3";"d4";"e5";"f6";"g7"]
             test <@ actualMoves = expectedMoves @>
@@ -64,14 +65,14 @@ module ``Queen movement`` =
     module ``When board is empty`` =
         [<Test>]
         let ``Returns correct moves from e3`` () =
-            let source, piece = "e3", { Player = White; Kind = Queen }
+            let source, piece = a White Queen at "e3"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece (["c1";"d2";"f4";"g5";"h6";"a7";"b6";"c5";"d4";"f2";"g1"] @ ["e1"; "e2"; "e4"; "e5"; "e6"; "e7"; "e8"; "a3"; "b3"; "c3"; "d3"; "f3"; "g3"; "h3"])
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Returns correct moves from h8`` () =
-            let source, piece = "h8", { Player = White; Kind = Queen }
+            let source, piece = a White Queen at "h8"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece (["a1";"b2";"c3";"d4";"e5";"f6";"g7"] @ ["h7";"h6";"h5";"h4";"h3";"h2";"h1";"g8";"f8";"e8";"d8";"c8";"b8";"a8"])
             test <@ actualMoves = expectedMoves @>
@@ -81,14 +82,14 @@ module ``Knight movement`` =
     module ``When board is empty`` =
         [<Test>]
         let ``Returns correct moves from e4`` () =
-            let source, piece = "e4", { Player = White; Kind = Knight }
+            let source, piece = a White Knight at "e4"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["d2";"d6";"f2";"f6";"c3";"c5";"g3";"g5"]
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Returns correct moves from g7`` () =
-            let source, piece = "g7", { Player = White; Kind = Knight }
+            let source, piece = a White Knight at "g7"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["f5";"h5";"e6";"e8";]
             test <@ actualMoves = expectedMoves @>
@@ -98,14 +99,14 @@ module ``King movement`` =
     module ``When board is empty`` =
         [<Test>]
         let ``Returns correct moves from e3`` () =
-            let source, piece = "e3", { Player = White; Kind = King }
+            let source, piece = a White King at "e3"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["d2";"e2";"f2";"d3";"f3";"d4";"e4";"f4";]
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Returns correct moves from h8`` () =
-            let source, piece = "h8", { Player = White; Kind = King }
+            let source, piece = a White King at "h8"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["h7";"g8";"g7";]
             test <@ actualMoves = expectedMoves @>
@@ -115,28 +116,44 @@ module ``Pawn movement`` =
     module ``When board is empty`` =
         [<Test>]
         let ``White pawn can move 2 squares from start position`` () =
-            let source, piece = "b2", { Player = White; Kind = Pawn }
+            let source, piece = a White Pawn at "b2"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["b3";"b4"]
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``White pawn can move only 1 square if not in start position`` () =
-            let source, piece = "f4", { Player = White; Kind = Pawn }
+            let source, piece = a White Pawn at "f4"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["f5";]
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Black pawn can move 2 squares from start position`` () =
-            let source, piece = "b7", { Player = Black; Kind = Pawn }
+            let source, piece = a Black Pawn at "b7"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["b6";"b5"]
             test <@ actualMoves = expectedMoves @>
 
         [<Test>]
         let ``Black pawn can move only 1 square if not in start position`` () =
-            let source, piece = "f4", { Player = Black; Kind = Pawn }
+            let source, piece = a Black Pawn at "f4"
             let actualMoves = availableMovesForPiece (boardWithPieces [source, piece]) (coord source) piece
             let expectedMoves = movesToTargetSquares source piece ["f3";]
+            test <@ actualMoves = expectedMoves @>
+
+    module ``When pawn is blocked`` = 
+        [<Test>]
+        let ``Pawn cannot move if blocked by same color piece`` () =
+            let source, piece = a White Pawn at "c2"
+            let board = boardWithPieces [source, piece; a White Rook at "c3"]
+            let actualMoves = availableMovesForPiece board (coord source) piece
+            let expectedMoves : Set<Move> = Set.empty
+            test <@ actualMoves = expectedMoves @>
+
+        let ``Pawn cannot move if blocked by enemy piece`` () =
+            let source, piece = a White Pawn at "c2"
+            let board = boardWithPieces [source, piece; a Black Rook at "c3"]
+            let actualMoves = availableMovesForPiece board (coord source) piece
+            let expectedMoves : Set<Move> = Set.empty
             test <@ actualMoves = expectedMoves @>
