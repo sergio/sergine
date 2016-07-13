e1de24dd392e8b31c2d401dd669adb807bdcb57f
Sergio Acosta <sergio.acosta@gmail.com>
2016-07-13 09:37:18 -0500

    New failing acceptance test: own king must not be in check after a move.

--- source/Sergine.Tests/AcceptanceTests.fs
+++ source/Sergine.Tests/AcceptanceTests.fs
@@ -20,3 +20,17 @@ let ``Engine suggests a valid move for white as the best move for the start posi
             | Success m -> Seq.exists ((=) m) validMovesForWhite
             | Failure _ -> false 
          @>
+
+[<Test>]
+[<Category("Acceptance")>]
+let ``Engine does not suggest a move that leaves the player's king in check`` () =
+
+    let position = "k7/bq6/8/8/6pp/8/6PP/7K w - - 0 1"
+    let expectedMove = "h2h3"
+    
+    let suggestedMove = Engine.bestMoveForPosition position
+
+    test <@ match suggestedMove with
+            | Success m -> m = expectedMove
+            | Failure _ -> false 
+         @>
